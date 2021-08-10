6a3a55ba56820ec255fd2bcecab3b3f7366911f1
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-08-10 16:13:03 +0600

    finally added tailwind in react

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="bg-gray-400 text-6xl p-2 text-white text-center">
+                This line style from tailwindcss .
+            </h1>
+        </div>
+    );
+};
+
+export default Header;
