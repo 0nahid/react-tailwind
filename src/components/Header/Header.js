e3ef9210b8948f82597681d73d7eb339a6b1eab6
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-08-10 18:49:12 +0600

    tested from android

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,9 +3,10 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            <h1 className="bg-gray-400 text-6xl p-2 text-white text-center">
+            <h1 className="bg-purple-400 text-6xl p-2 text-white text-center">
                 This line style from tailwindcss .
             </h1>
+            <h1 className="text-left text-bold text-red-700 text-3xl "> Test from Android &  It's working nicely.</h1>
         </div>
     );
 };
