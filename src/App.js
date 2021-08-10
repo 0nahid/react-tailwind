4e1f9220c226d082d7623730c4ca9b74a3db7882
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-08-11 02:06:39 +0600

    finished site

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import Class from './components/Class/Class';
 import Header from './components/Header/Header';
+import Courses from './components/Courses/Courses';
+import Pricing from './components/Pricing/Pricing';
+import Hero from './components/Hero/Hero';
+import Contact from './Contact/Contact';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div>
       <Header></Header>
       <Class></Class>
+      <Courses></Courses>
+      <Pricing></Pricing>
+      <Hero></Hero>
+      <Contact></Contact>
+      <Footer></Footer>
     </div>
   );
 }
