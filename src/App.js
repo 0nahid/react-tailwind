41ba9c581f349829ae44d4bf152c234e6773039c
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-08-10 23:15:51 +0600

    added header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Class from './components/Class/Class';
 import Header from './components/Header/Header';
 function App() {
   return (
     <div>
       <Header></Header>
+      <Class></Class>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,29 @@
 import React from 'react';
-
 const Header = () => {
     return (
-        <div>
-            <h1 className="bg-purple-400 text-6xl p-2 text-white text-center">
-                This line style from tailwindcss .
-            </h1>
-            <h1 className="text-left text-bold text-red-700 text-3xl "> Test from Android &  It's working nicely.</h1>
-        </div>
+        <header className="text-gray-600 body-font">
+            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                <nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
+                    <a href="/" className="mr-5 hover:text-gray-900">Home</a>
+                    <a href="/" className="mr-5 hover:text-gray-900">Courses</a>
+                    <a href="/" className="mr-5 hover:text-gray-900">Review</a>
+                    <a href="/" className="hover:text-gray-900">My Learning</a>
+                </nav>
+                <a href="/" className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
+                    <svg style={{ color: 'green' }} xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
+                    </svg>
+                    <span className="ml-3 text-xl">NDemy</span>
+                </a>
+                <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
+                    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Login
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </header>
     );
 };
 
